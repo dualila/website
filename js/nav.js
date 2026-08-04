d99df9fd015783648ba82c483e65bcc481484bea
dualila <lilywakefield30@gmail.com>
2026-08-05 00:13:56 +1000

    add wheel

--- js/nav.js
+++ js/nav.js
@@ -27,6 +27,7 @@ const NAV_TREE = [
       { label: "the orb", href: "/orb/orb.html"},
       { label: "tram simulator", href: "/tram/tram.html"},
       { label: "the wall", href: "/wall.html" },
+      { label: "wheel of indecision", href: "/wheel.html" },
     ]
   },
     { 
